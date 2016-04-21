88a64b6f67ecfca95e67b435483e858dafa47c82
z-gora <domdlapsa@gmail.com>
2016-04-21 23:04:11 +0200

    Added files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var godziny = [[0, "22:45", "22:48", "22:51", "22:54", "22:57", "23:0", "23:3", "23:6", "23:9", "23:12"],[0, "22:46", "22:49", "22:52", "22:55", "22:58", "23:1", "23:4", "23:7", "23:10", "23:13"],[0, "22:47", "22:50", "22:53", "22:56", "22:59", "23:2", "23:5", "23:8", "23:11", "23:14"]];
+var godziny = [[0, "22:45", "22:48", "22:51", "22:54", "22:57", "23:00", "23:03", "23:06", "23:09", "23:12"],[0, "22:46", "22:49", "22:52", "22:55", "22:58", "23:01", "23:04", "23:07", "23:10", "23:13"],[0, "22:47", "22:50", "22:53", "22:56", "22:59", "23:02", "23:05", "23:08", "23:11", "23:14"]];
 $(document).ready(function() {
     var d = new Date();
     currentTime = timeParser(addZero(d.getHours()).toString() + ":" + addZero(d.getMinutes()).toString());
@@ -21,16 +21,20 @@ $(document).ready(function() {
         if(prev!=d.getMinutes()) {
             for (i=0; i<godziny.length; i++) {
                 for (j=1; j<godziny[i].length; j++) {
-                    if (timeParser(godziny[i][j]) >= currentTime) break;
+                    if (timeParser(godziny[i][j]) >= currentTime) {
+                        nextOne = j;
+                        break;
+                    }
                 }
+                //alert(i+" "+nextOne);
                 if(j==godziny[i].length) {
                     $("#part"+i+" .countdown span").text("--");
                 } else {
-                    left = timeParser(godziny[i][j])-currentTime;
+                    left = timeParser(godziny[i][nextOne])-currentTime;
                     if (left==0) {
-                        wyjscie(i, addZero(left));
+                        wyjscie(i, left);
                     } else {
-                        changeTime(i, addZero(left));
+                        changeTime(i, left);
                     }
                     
                 }
@@ -46,7 +50,7 @@ $(document).ready(function() {
     }
     function changeTime(i, value) {
         $("#part"+i+" .countdown span").animate({opacity: 0}, 1000, function() {
-            $(this).text(addZero(timeParser(godziny[i][j])-currentTime));
+            $(this).text(value);
             $(this).animate({opacity: 1}, 1000);
         })
     }
